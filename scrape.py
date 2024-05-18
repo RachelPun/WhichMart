66746a2f20144b8e803d54ca9771744a213c88f3
RachelPun <rachelpunhokyan@ymail.com>
2024-05-18 10:42:33 +0100

    scraped multiple items on 1 page

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         "User-Agent": "Mozilla/5.0 (Linux Android 6.0 Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"}
 
     response = requests.get(
-        "https://www.tesco.com/groceries/en-GB/search?query=apples&department=Fresh%20Fruit&viewAll=department&page=2",
+        "https://www.tesco.com/groceries/en-GB/search?query=apples&department=Fresh%20Fruit&viewAll=department",
         headers=headers)
 
     soup = BeautifulSoup(response.text, features="html.parser")
@@ -17,10 +17,11 @@ if __name__ == "__main__":
     current = divs.find(
         "div", class_="search product-list--page product-list--current-page")
     for wrapper in current.find_all("div", class_="product-details--wrapper"):
-        product = wrapper.find(
-            "span", class_=re.compile(r"styled__Text.*link__text")).text
         price_box = wrapper.find(
             "div", class_=re.compile(r"base-components__RootElement.*price__container"))
-        price = price_box.find(
-            "p", class_=re.compile(r"styled__StyledHeading.*price__text.*")).text
-        print(product, price)
+        if price_box:
+            price = price_box.find(
+                "p", class_=re.compile(r"styled__StyledHeading.*price__text.*")).text
+            product = wrapper.find(
+                "span", class_=re.compile(r"styled__Text.*link__text")).text
+            print(product, price)
